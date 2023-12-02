3c47552fefd0cdb2bfc038e5b98cff471880b73f
emoshi02 <emilijarack49@gmail.com>
2023-12-02 13:46:41 +0200

    Add icon navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,15 +11,39 @@ const Navigation = () => {
         <Link to={'/About'}>
           <li className="nav-item">Apie</li>
         </Link>
-        <li className="nav-item">CV</li>
+        <li
+          className="nav-item"
+          onClick={() =>
+            window.open(
+              'https://drive.google.com/file/d/1eecz71GD59uQEKrB8UUV8xxXMJpVyLS8/view?fbclid=IwAR1Tzd19tlRSoUhk124rVEOZw5y68Pf87oOZbSOQrEVIJzdsvg22C1FTt44',
+            )
+          }
+        >
+          CV
+        </li>
 
-        <li className="icon">
+        <li
+          className="icon"
+          onClick={() => window.open('mailto:emilijarack49@gmail.com')}
+        >
           <EmailLogo />
         </li>
-        <li className="icon">
+        <li
+          className="icon"
+          onClick={() =>
+            window.open(
+              ' https://www.linkedin.com/in/emilija-ra%C4%8Dkauskait%C4%97-173449223/',
+            )
+          }
+        >
           <LinkedinLogo />
         </li>
-        <li className="icon">
+        <li
+          className="icon"
+          onClick={() =>
+            window.open('https://www.facebook.com/emile.rackauskaite/')
+          }
+        >
           <FacebookLogo />
         </li>
       </ul>
